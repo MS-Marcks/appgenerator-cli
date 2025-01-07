f3e7520673972f50b141fd148863b0250b7f6486
MS-Marcks <gersonramos46@gmail.com>
2025-01-07 13:39:34 -0600

    feat: noveno

--- dist/utils/read.js
+++ dist/utils/read.js
@@ -6,10 +6,12 @@ Object.defineProperty(exports, "__esModule", {
 exports.readJSONInternal = exports.readJSON = void 0;
 var _fs = _interopRequireDefault(require("fs"));
 var _path = _interopRequireDefault(require("path"));
-var _url = _interopRequireDefault(require("url"));
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
+//import url from 'url';
+
 const baseDir = _path.default.resolve(".");
-const rootDirectory = _path.default.join(_path.default.dirname(_url.default.fileURLToPath(import.meta.url)), "..", "..");
+const rootDirectory = _path.default.join(__dirname, "..", "..");
+console.log(rootDirectory);
 const readJSON = (filePath, root = null) => {
   try {
     const data = _fs.default.readFileSync(_path.default.join(root === null ? baseDir : root, filePath), 'utf-8');
